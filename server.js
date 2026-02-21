598011669d53a6dada2d681ad7115fc259230138
vijaybalaji516 <vijaybalaji516@gmail.com>
2026-02-21 17:10:29 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Route for index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Portfolio running at http://localhost:${PORT}`);
+});
